9ccb60a063a78b8117829002a80b9ddab985f883
anaggomes <157487724+anaggomes@users.noreply.github.com>
2024-05-07 14:37:35 +0100

    creates articleComments component, adds button to show more comments, adds allArticles link to header

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,3 +1,26 @@
+import { Link } from "react-router-dom";
+import AllArticles from "./AllArticles";
+
 export default function Header() {
-  return <section id="header">Header</section>;
+  return (
+    <section id="header">
+      <h1 id="app-title">NC NEWS</h1>
+      <nav className="navBar">
+        <ul>
+          {/* <Link to="/"> */}
+          {/* <li>Home</li> */}
+          {/* </Link> */}
+          <Link to="/articles" element={<AllArticles />}>
+            <li>All Articles</li>
+          </Link>
+          {/* <Link to="/topics"> */}
+          {/* <li>All Topics</li> */}
+          {/* </Link> */}
+          {/* <Link to="/users"> */}
+          {/* <li>All Users</li> */}
+          {/* </Link> */}
+        </ul>
+      </nav>
+    </section>
+  );
 }
